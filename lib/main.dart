3aa8be4f3ece821f17485c016aac1bb3fa8bccec
manoj1995madushanka <madushankamanoj414@gmail.com>
2021-06-13 04:52:42 +0530

    add name and mail

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white30,
+        backgroundColor: Colors.cyan,
         body: SafeArea(
           child: Column(
             children: [
@@ -25,18 +25,67 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                     fontFamily: 'Pacifico', // font family in pubSpec
                     fontWeight: FontWeight.bold,
-                    fontSize: 40.0,
+                    fontSize: 30.0,
                     color: Colors.white),
               ),
               Text(
                 'Software Engineer',
                 style: TextStyle(
-                    fontFamily: 'Source Sans Pro', // font family in pubSpec
+                    fontFamily: 'Source Sans Pro',
+                    // font family in pubSpec
                     fontWeight: FontWeight.bold,
                     fontSize: 20.0,
                     color: Colors.teal[100],
-                letterSpacing: 2.5),
-              )
+                    letterSpacing: 2.5),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.symmetric(horizontal: 5.0,vertical: 2.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      size: 20.0,
+                      color: Colors.amber,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '0712345678',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal.shade900),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.symmetric(horizontal: 5.0,vertical: 2.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      size: 20.0,
+                      color: Colors.amber,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'madushankamanoj414@gmail.com',
+                      style: TextStyle(
+                          fontSize: 18.0,
+                          fontFamily: 'Source Sans Pro',
+                          color: Colors.teal.shade900),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
